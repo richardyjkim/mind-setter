0b8831bf23643bf1ffb76edd3c8ed2928adf5fdc
Richard <richardyjkim@gmail.com>
2021-05-06 22:59:06 -0700

    user-route constructed

---
+++ controller/api/user-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { User, Post, Comment, Like } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'content', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Like,
+        as: 'liked_posts'
+      }
+    ]
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const sequelize = require("./config/connection");
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // const sess = {
-//   secret: 'Ground ten project',
+//   secret: 'Group ten project',
 //   cookie: { maxAge: 36000 },
 //   resave: false,
 //   saveUninitialized: true,
