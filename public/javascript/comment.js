ba02ecbd261e8f19ee497cd351549b62bd248269
AustinJohnAaro <austin1472a@gmail.com>
2021-05-11 21:11:10 -0700

    made javascript from modles

---
+++ public/javascript/comment.js
@@ -0,0 +1,30 @@
+async function commentsFormHandler(event) {
+    event.preventDefault();
+
+    // const title = document.querySelector('input[name="post-title"]').value;
+    // const post_url = document.querySelector('input[name="post-url"]').value;  
+
+
+
+
+    const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({
+            post_id,
+            comment_text,
+
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+    // document.querySelector('.new-post-form').addEventListener('submit', commentsFormHandler);
+
