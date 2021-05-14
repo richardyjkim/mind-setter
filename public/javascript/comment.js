ebc95d399797c4d469d5d1a5bf578e806377abbd
Richard <richardyjkim@gmail.com>
2021-05-14 10:49:02 -0700

    comment counte constructed

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,25 +1,32 @@
-async function commentsFormHandler(event) {
+async function commentFormHandler(event) {
   event.preventDefault();
 
-  // const title = document.querySelector('input[name="post-title"]').value;
-  // const post_url = document.querySelector('input[name="post-url"]').value;  
+  const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
 
-  const response = await fetch(`/api/comments`, {
-    method: 'POST',
-    body: JSON.stringify({
-      post_id,
-      comment_text,
-    }),
-    headers: {
-      'Content-Type': 'application/json'
+  const post_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  if (comment_text) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        post_id,
+        comment_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      document.location.reload();
+
+    } else {
+      alert(response.statusText);
+      document.querySelector('#comment-form').style.display = "block";
     }
-  });
-  if (response.ok) {
-    document.location.reload();
-  } else {
-    alert(response.statusText);
   }
 }
 
-    // document.querySelector('.new-post-form').addEventListener('submit', commentsFormHandler);
-
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
